0774b6b8dc061f4a24561e553653dc9630e0c679
punithsabitha <sabithadm36@gmail.com>
2026-04-21 20:43:18 +0530

    Add files via upload

---
+++ intersectionofset.py
@@ -0,0 +1,14 @@
+# create the two sets of two guest lists
+set1 = {'A','B','C','D','E'}
+set2 = {'B','D','V','X','Y','Z'}
+
+# find union of two sets
+union = set1.intersection(set2)
+
+# converting set into list
+# to find total guests to be invited in the party
+
+total_guests = list(union)
+
+print("Total guests to be invited in the party are:", len(total_guests))
+print("Guest List", total_guests)
